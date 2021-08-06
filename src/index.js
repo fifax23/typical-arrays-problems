5a8277f59218dc5dc1266d8ea651500c82c7188a
fifax23 <galaxymexx@gmail.com>
2021-08-06 21:49:09 +0600

    first version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    let result = 0;
+    if (array && array.length > 0) {
+        result = Math.min(...array);
+    }
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let result = 0;
+    if (array && array.length > 0) {
+        result = Math.max(...array);
+    }
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let result = 0;
+    if (array && array.length > 0) {
+        result = array.reduce((prev, curr) => prev + curr) / array.length;
+    }
+    return result;
+};
